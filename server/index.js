74a0a72b018de2e96a5d938b73e7224a5bdcf792
carlagidacan <carlagidacan2917@gmail.com>
2026-03-08 01:25:49 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -17,12 +17,19 @@ let dbConnectionPromise
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'DTR backend is running',
+    health: '/api/test'
+  })
+})
+
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Server Connected Successfully!' })
 })
 
 app.use(async (req, res, next) => {
-  if (req.path === '/api/test') {
+  if (req.path === '/' || req.path === '/api/test') {
     next()
     return
   }
